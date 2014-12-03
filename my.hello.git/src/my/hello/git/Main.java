eca3e42cfb44b9d54524b7d87e891e0bab53a586
Yehuda Ginsburg <yehudalg@gmail.com>
2014-12-03 15:08:38 +0200

    Add fixit

--- my.hello.git/src/my/hello/git/Main.java
+++ my.hello.git/src/my/hello/git/Main.java
@@ -2,7 +2,7 @@ package my.hello.git;
 
 public class Main {
 	public static void main(String[] args) {
-		Person p = new Person();
-		System.out.println(p);
+		Person p1 = new Person();
+		System.out.println(p1);
 	}
 }
